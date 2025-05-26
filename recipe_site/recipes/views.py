61c2c9c14b828080586656547b50561308382224
MaryaSerikova <153833244+Maryshka228@users.noreply.github.com>
2025-05-26 11:55:13 +0300

    Add files via upload

---
+++ recipe_site/recipes/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.contrib import messages
+from .models import Recipe
+from .forms import RecipeForm, RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+
+def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/home.html', {'recipes': recipes})
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+@login_required
+def edit_recipe(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk, author=request.user)
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('recipe_detail', pk=pk)
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, 'recipes/add_recipe.html', {'form': form})
+
+@login_required
+def delete_recipe(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk, author=request.user)
+    if request.method == 'POST':
+        recipe.delete()
+        return redirect('home')
+    return render(request, 'recipes/confirm_delete.html', {'recipe': recipe})
+
+@login_required
+def add_recipe(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.save()
+            form.save_m2m()
+            return redirect('home')
+    else:
+        form = RecipeForm()
+    return render(request, 'recipes/add_recipe.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'recipes/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'recipes/login.html', {
+        'form': form,
+        'errors': form.errors.items() if form.errors else None
+    })
