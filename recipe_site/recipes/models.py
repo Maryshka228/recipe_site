1ce89416b8d237e8fe69d8c2756286f9e9ae2160
MaryaSerikova <153833244+Maryshka228@users.noreply.github.com>
2025-05-31 14:43:01 +0300

    Update models.py

--- recipe_site/recipes/models.py
+++ recipe_site/recipes/models.py
@@ -11,13 +11,13 @@ class Category(models.Model):
     
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=255)  # Название рецепта
-    description = models.TextField()  # Описание
-    steps = models.TextField()  # Шаги приготовления
-    cooking_time = models.PositiveIntegerField(help_text="Время в минутах")  # Время приготовления
-    image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)  # Картинка
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Автор рецепта
-    categories = models.ManyToManyField(Category, related_name="recipes")  # Категории
+    title = models.CharField(max_length=255)  
+    description = models.TextField()  
+    steps = models.TextField() 
+    cooking_time = models.PositiveIntegerField(help_text="Время в минутах") 
+    image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)  
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  
+    categories = models.ManyToManyField(Category, related_name="recipes")  
     
     def __str__(self):
         return self.title
